46788176165c8128a28724b94ffcf9578fbe3d03
KID <50666207+KID421@users.noreply.github.com>
2019-10-17 12:22:04 +0800

    完成

---
+++ UnityProject/Assets/eatting_ctrl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eatting_ctrl : MonoBehaviour {
+
+    Animator m_Animator;
+    // Use this for initialization
+    void Start () {
+		m_Animator = gameObject.GetComponent<Animator>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            m_Animator.SetTrigger("吃飯觸發器");
+           
+        }
+      
+
+        
+        if (Input.GetKey(KeyCode.R))
+        {
+            m_Animator.SetBool("跑步開關", true);
+        }
+        else 
+        {
+            m_Animator.SetBool("跑步開關", false);
+        }
+    }
+}
