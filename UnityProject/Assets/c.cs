a6793707c352f34f8aa94171d49413e6da018554
raugh135 <55518299+raugh135@users.noreply.github.com>
2019-10-24 10:03:03 +0800

    chicken

---
+++ UnityProject/Assets/c.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c : MonoBehaviour {
+    private Animator ani;
+
+    // Use this for initialization
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ani.SetTrigger("吃飯觸發器");
+        }
+
+        ani.SetBool("跑步開關", Input.GetKey(KeyCode.R));
+    }
+}
